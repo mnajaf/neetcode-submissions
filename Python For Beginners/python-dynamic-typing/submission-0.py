e3d645f56533565cd56c9167d800ad61053f9c8e
mnajaf <124029217+mnajaf@users.noreply.github.com>
2026-08-29 00:33:28 -0400

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 10.0
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "hi"
+print(type(variable))
+variable = [1,2]
+print(type(variable))
